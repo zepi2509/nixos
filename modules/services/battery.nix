4092dab4e12d3ad654bb13ecda2d5e56a49e68f8
Noah Zepner <noah@zepner.dev>
2025-04-23 22:36:40 +0200

    chore: update battery notifier

--- modules/services/battery.nix
+++ modules/services/battery.nix
@@ -10,7 +10,7 @@
     description = "Batsignal Battery Monitor";
     wantedBy = [ "default.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.batsignal}/bin/batsignal -w 21 -c 10 -d 5 -W \"Battery at 20%\" -C \"Battery low at 10%\" -D \"Battery critical at 5%\"";
+      ExecStart = "${pkgs.batsignal}/bin/batsignal -w 20 -c 10 -d 5 -W \"Battery at 20%\" -C \"Battery low at 10%\" -D \"Battery critical at 5%\"";
       Restart = "on-failure";
       RestartSec = "5";
     };
