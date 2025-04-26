4eacf20fa4d7e257eb23e6eb8dd0a78737894812
Noah Zepner <noah@zepner.dev>
2025-04-26 19:59:39 +0200

    update shell config

--- users/zepi/applications/ghostty.nix
+++ users/zepi/applications/ghostty.nix
@@ -3,5 +3,14 @@
 {
   programs.ghostty = {
     enable = true;
+    settings = {
+      cursor-style = "underline";
+      shell-integration-features = "no-cursor";
+
+      gtk-tabs-location = "left";
+
+      keybind = [
+      ];
+    };
   };
 }
