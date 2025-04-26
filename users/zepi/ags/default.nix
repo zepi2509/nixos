9262409b85f1c42127628008ad37114bbc6ec886
Noah Zepner <noah@zepner.dev>
2025-04-26 17:30:03 +0200

    fix: fix with pkgs on ags modules

--- users/zepi/ags/default.nix
+++ users/zepi/ags/default.nix
@@ -9,10 +9,10 @@
     configDir = ./conf;
 
     extraPackages = with pkgs; [
-      inputs.ags.packages.${pkgs.system}.battery
-      inputs.ags.packages.${pkgs.system}.wireplumber
-      inputs.ags.packages.${pkgs.system}.network
-      inputs.ags.packages.${pkgs.system}.bluetooth
+      inputs.ags.packages.${system}.battery
+      inputs.ags.packages.${system}.wireplumber
+      inputs.ags.packages.${system}.network
+      inputs.ags.packages.${system}.bluetooth
     ];
   };
 }
