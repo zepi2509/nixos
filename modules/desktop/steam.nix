e73d3781d67acb4f68b8226efaba28c7bd085439
Noah Zepner <noah@zepner.dev>
2025-06-07 20:44:04 +0200

    feat: add steam

---
+++ modules/desktop/steam.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    protontricks = {
+      enable = true;
+    };
+  };
+}
