cbd58f824f6d1ddbab6f4d15ee213e5f33a23c84
Noah Zepner <noah@zepner.dev>
2025-08-02 15:05:45 +0200

    chore: ags

--- flake.nix
+++ flake.nix
@@ -1,5 +1,4 @@
 {
-  # Import other flakes
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager = {
@@ -8,10 +7,6 @@
     };
     hyprland = {
       url = "github:hyprwm/Hyprland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    ags = {
-      url = "github:aylur/ags";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser = {
@@ -30,18 +25,14 @@
       url = "github:gmodena/nix-flatpak";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    fh-kiel-vpn = {
-      url = "github:zepi2509/fh-kiel-vpn-flake";
     };
   };
 
   outputs = { nixpkgs, ... }@inputs: 
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
   in
   {
-    # Define new hosts here
     nixosConfigurations = {
       "ZEPI-Notebook" = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs system; };
