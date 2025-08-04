196c11dc764dbc4fa22ce5761dc3a57b25c08f87
Noah Zepner <noah@zepner.dev>
2025-08-04 14:17:21 +0200

    add: printing

---
+++ modules/services/printing.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+  services = {
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
+    printing = {
+      enable = true;
+      drivers = with pkgs; [
+        cups-filters
+        cups-browsed
+        epson-escpr2
+        epson-escpr
+      ];
+    };
+  };
+}
