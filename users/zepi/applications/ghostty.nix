7b49c0a8f1e52660a10356e3b57795c321da81f8
Noah Zepner <noah@zepner.dev>
2025-10-26 16:02:07 +0100

    update ghostty config

--- users/zepi/applications/ghostty.nix
+++ users/zepi/applications/ghostty.nix
@@ -7,8 +7,6 @@
       cursor-style = "underline";
       shell-integration-features = "no-cursor";
 
-      gtk-tabs-location = "left";
-
       window-padding-x = "15";
       window-padding-y = "15";
       window-padding-balance = "true";
