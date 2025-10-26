070645371653b5ba716751e232a8fe2efa03160f
Noah Zepner <noah@zepner.dev>
2025-10-26 16:16:42 +0100

    chore: remove onedrive override

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,9 +1,9 @@
 final: prev:
 let
   # zen = import ./zen.nix final prev;
-  onedrive = import ./onedrive.nix final prev;
+  # onedrive = import ./onedrive.nix final prev;
 in
 {
   # inherit (zen) zen-browser;
-  inherit (onedrive) onedrive;
+  # inherit (onedrive) onedrive;
 }
