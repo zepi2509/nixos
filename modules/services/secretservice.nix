d828f721767471e943ad4b40ef5c89a7fff67024
Noah Zepner <noah@zepner.dev>
2025-12-19 16:35:53 +0100

    fix: fix gnomeKeyring not unlocking on login

--- modules/services/secretservice.nix
+++ modules/services/secretservice.nix
@@ -1,6 +1,15 @@
-{...}:
+{ pkgs, ... }:
 
 {
+  programs.seahorse.enable = true;
   services.gnome.gnome-keyring.enable = true;
-  security.pam.services.hyprland.enableGnomeKeyring = true;
+  security.pam.services = {
+    greetd.enableGnomeKeyring = true;
+    greetd-password.enableGnomeKeyring = true;
+    login.enableGnomeKeyring = true;
+  };
+  services.dbus.packages = with pkgs; [
+    gnome-keyring
+    gcr
+  ];
 }
