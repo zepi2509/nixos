59d6d10c30f4ab341c89c3983561f867b3fee5b6
Noah Zepner <noah@zepner.dev>
2025-12-19 17:34:44 +0100

    fix: nvim nixfmt

--- users/zepi/nvim.nix
+++ users/zepi/nvim.nix
@@ -1,13 +1,25 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   dotfiles = config.lib.file.mkOutOfStoreSymlink ./.dotfiles;
 in
-  {
+{
   programs.neovim = {
     enable = true;
     extraLuaConfig = lib.fileContents "${dotfiles}/nvim/init.lua";
-    extraLuaPackages = extraPkgs: with extraPkgs; [
-      luarocks   ];
+    extraLuaPackages =
+      extraPkgs: with extraPkgs; [
+        luarocks
+      ];
+    extraPackages = with pkgs; [
+      nil
+      nixfmt
+      statix
+    ];
   };
 
   xdg.configFile = {
