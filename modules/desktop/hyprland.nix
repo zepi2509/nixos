9fb5afc39f5acba5e3191b9f41fa5650f1c3a598
Noah Zepner <noah@zepner.dev>
2025-12-20 04:57:23 +0100

    use uwsm

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -9,7 +9,8 @@
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     withUWSM = true;
     xwayland.enable = true;
   };
