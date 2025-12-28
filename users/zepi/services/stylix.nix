4c7be8440032de3077633bc5ae3b90aa13c788df
Noah Zepner <noah@zepner.dev>
2025-12-28 18:26:59 +0100

    upd: stylix

--- users/zepi/services/stylix.nix
+++ users/zepi/services/stylix.nix
@@ -1,21 +1,11 @@
 { pkgs, ... }:
 {
   stylix = {
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/classic-dark.yaml";
-    polarity = "dark";
     opacity = {
       applications = 0.8;
       desktop = 0.8;
       popups = 0.8;
       terminal = 0.8;
     };
-
-    iconTheme = {
-      enable = true;
-      package = pkgs.adwaita-icon-theme;
-      light = "Adwaita";
-      dark = "Adwaita";
-    };
   };
-
 }
