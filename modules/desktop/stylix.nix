ef0ad2add4fb2fa7b179ce6ccaeda92e26ef4c60
Noah Zepner <noah@zepner.dev>
2026-01-24 21:41:55 +0100

    chore: theme

--- modules/desktop/stylix.nix
+++ modules/desktop/stylix.nix
@@ -9,6 +9,25 @@
     enable = true;
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark-dark.yaml";
+
+    override = {
+      # Syntax Accents
+      base08 = "d4a4a4"; # $red
+      base09 = "c79c74"; # $orange
+      base0A = "c1c4ca"; # $gray (Often used for specific UI/Yellow roles)
+      base0B = "9aaf8b"; # $green
+      base0C = "83b2b6"; # $cyan
+      base0D = "779dc9"; # $blue
+      base0E = "b6addb"; # $purple
+
+      # Foreground/Text
+      base05 = "c1c4ca"; # $gray
+
+      # Optional: Handling the "illegal-red" or "interpolation"
+      # Usually mapped to base0F in Base16 for special markup
+      base0F = "b87777";
+    };
+
     polarity = "dark";
 
     cursor = {
